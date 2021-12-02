2a24e88b18499131b5cf3767ba8f2f7be6405c4c
andrewburroughs <andrewburroughs@users.noreply.github.com>
2021-12-02 18:20:00 +0000

    deploy: b3375745d95c7cc155e044880357b60ba1b4df07

--- search/all_a.js
+++ search/all_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['markersubpixelparallel_0',['MarkerSubpixelParallel',['../classcv_1_1aruco_1_1MarkerSubpixelParallel.html',1,'cv::aruco']]]
+  ['mainpage_2ecpp_0',['MainPage.cpp',['../MainPage_8cpp.html',1,'']]],
+  ['markersubpixelparallel_1',['MarkerSubpixelParallel',['../classcv_1_1aruco_1_1MarkerSubpixelParallel.html',1,'cv::aruco']]]
 ];
